c878a42d917e62eaaae0a8119b98910bbc625840
serge <sergei.g.d@gmail.ccom>
2022-07-26 14:54:53 +0700

    1

---
+++ src/main/java/pro/sky/skyprowebemployee/SkyproWebEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprowebemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproWebEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproWebEmployeeApplication.class, args);
+	}
+
+}
